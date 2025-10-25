942c4f47536c22f2a9ab0644b0ec3a7ff5bda13b
Anish Kumar <122anish.kumar@gmail.com>
2025-10-25 15:32:33 -0700

    made changes to logic for standard decoder, need to create loss-aware decoder

--- True Simulation Program for Fault-Tolerant Decoder.py
+++ True Simulation Program for Fault-Tolerant Decoder.py
@@ -8,11 +8,11 @@ from qiskit_aer.noise.errors import kraus_error
 
 # --- Step 1: Build a simple repetition code (encode |ψ> into 3 qubits) ---
 def CircuitParameters():
-    QuantumCircuit = QuantumCircuit(3, 3)
-    QuantumCircuit.h(0)             # Start with |+> = (|0>+|1>)/√2 as logical state
-    QuantumCircuit.cx(0, 1)         # Encode
-    QuantumCircuit.cx(0, 2)
-    return QuantumCircuit
+    qc = QuantumCircuit(3, 3)
+    qc.h(0)             # Start with |+> = (|0>+|1>)/√2 as logical state
+    qc.cx(0, 1)         # Encode
+    qc.cx(0, 2)
+    return qc
 
 
 # look at ancillas & syndrome extraction if time
@@ -23,11 +23,11 @@ def CircuitParameters():
 # that describe the evolution of an open quantum system, 
 # such as a quantum state interacting with an environment
 
-p_loss = 0.2  # loss probability
+ploss = 0.2  # loss probability
 
 
-K0 = np.array([[1, 0], [0, np.sqrt(1 - p_loss)]])
-K1 = np.array([[0, np.sqrt(p_loss)], [0, 0]])
+K0 = np.array([[1, 0], [0, np.sqrt(1 - ploss)]])
+K1 = np.array([[0, np.sqrt(ploss)], [0, 0]])
 loss_error = kraus_error([K0, K1])
 #Quantum Error Object
 
@@ -36,29 +36,34 @@ loss_error = kraus_error([K0, K1])
 #100% probability satisfied array [1,0], [0,1] is identity
 
 noise_model = NoiseModel()
-noise_model.add_all_qubit_quantum_error(loss_error, ["id"])
+noise_model.add_all_qubit_quantum_error(loss_error, ["cx", "h"])
 
 #id  means that the circuit will automatically apply the error to all processes, like idle instead of identity
 # --- Step 3: Simulate with noise ---
 simulator = AerSimulator(noise_model = noise_model)
 
-QuantumCircuit = CircuitParameters()
+qc = CircuitParameters()
 #check what this is
-QuantumCircuit.barrier()
-QuantumCircuit.measure([0, 1, 2], [0, 1, 2])  # measure all
+qc.barrier()
+qc.measure([0, 1, 2], [0, 1, 2])  # measure all
 #end check
 
-TranspiledQuantumCircuit = transpile(QuantumCircuit, simulator)
+TranspiledQuantumCircuit = transpile(qc, simulator)
 result = simulator.run(TranspiledQuantumCircuit, shots=2000).result()
 counts = result.get_counts()
 print("Raw measurement results with qubit loss:", counts)
+
+
 
 # --- Step 4: Define decoders ---
 def standard_decoder(counts):
     """Majority vote, assumes no loss"""
     logical_counts = {"0":0, "1":0}
     for outcome, freq in counts.items():
-        majority = "0" if outcome.count("0") > 1 else "1"
+        if outcome.count("0") > 1:
+            majority = "0"  
+        else:
+            majority = "1"
         logical_counts[majority] += freq
     return logical_counts
 
